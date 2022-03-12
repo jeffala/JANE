2686c1e51c3d58eb99417f9187a92aca7c5a1643
Jeff Licona <57239224+jeffala@users.noreply.github.com>
2022-03-12 15:28:40 -0500

    intial submission

---
+++ JANE/JANEApp.swift
@@ -0,0 +1,17 @@
+//
+//  JANEApp.swift
+//  JANE
+//
+//  Created by Jeff Licona on 3/8/22.
+//
+
+import SwiftUI
+
+@main
+struct JANEApp: App {
+    var body: some Scene {
+        WindowGroup {
+            BioView()
+        }
+    }
+}
---
+++ JANE/WorksView.swift
@@ -0,0 +1,43 @@
+
+import SwiftUI
+
+
+struct WorksView: View {
+    @State private var items = [Item]()
+    
+    var body: some View {
+        List(items, id: \.id) { item in
+            HStack {
+                Text(item.volumeInfo.title)
+                AsyncImage(url: URL(string: item.volumeInfo.imageLinks.smallThumbnail))
+            }
+            
+        }
+        .task {
+            await loadData()
+        }
+        .navigationTitle("Books")
+        .navigationBarTitleDisplayMode(.inline)
+    }
+    
+    func loadData() async {
+        guard let url = URL(string: "https://www.googleapis.com/books/v1/users/112081002230780611545/bookshelves/1001/volumes") else {
+            print("Invalid URL")
+            return
+        }
+        do {
+            let (data, _) = try await URLSession.shared.data(from: url)
+            if let decodedResponse = try? JSONDecoder().decode(Books.self, from: data) {
+                items = decodedResponse.items
+            }
+        } catch {
+            print("Invalid data")
+        }
+    }
+}
+
+struct WorksListView_Previews: PreviewProvider {
+    static var previews: some View {
+        WorksView()
+    }
+}
