6938d8a2d89406554944709577a84d77a1f9a58c
Jeff Licona <57239224+jeffala@users.noreply.github.com>
2022-03-28 20:22:58 -0400

    better info and final icon

--- JANE/Views/BookDetailViews.swift
+++ JANE/Views/BookDetailViews.swift
@@ -6,192 +6,32 @@ struct BookDetailView: View {
     
     var body: some View {
         ScrollView {
-        VStack {
-            Spacer()
-            VStack(spacing: 15) {
-                AsyncImage(url: localBookInfo.url) { returnedImage in
-                    returnedImage.resizable()
-                        .scaledToFit()
-                        .frame(width: 200, height: 250)
-                } placeholder: {
-                    ProgressView()
+            VStack {
+                Spacer()
+                VStack(spacing: 15) {
+                    AsyncImage(url: localBookInfo.url) { returnedImage in
+                        returnedImage.resizable().frame(width: 128, height: 197)
+                    } placeholder: {
+                        ProgressView()
+                    }
+                    Text(localBookInfo.title)
+                        .font(.title)
                 }
-
-                Text(localBookInfo.title)
-                    .font(.title)
+                Spacer()
+                Text(localBookInfo.description)
+                    .multilineTextAlignment(.center)
+                    .lineSpacing(10)
+                    .padding()
+                    .font(.title2)
+                Spacer()
+                Spacer()
             }
-            Spacer()
-            Text(localBookInfo.description)
-                .multilineTextAlignment(.center)
-                .lineSpacing(10)
-                .padding()
-                .font(.title2)
-            Spacer()
-            Spacer()
-        }
         }
     }
 }
-
 
 struct BookDetailView_Previews: PreviewProvider {
     static var previews: some View {
         BookDetailView(localBookInfo: BookDetails.topWorks.first!)
     }
 }
-
-
-//struct PersuasionDetailView: View {
-//    @ObservedObject var viewModel = WorksViewModel()
-//    let url = URL(string: "https://books.google.com/books/content?id=9IQqEAAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api")
-//
-//    var body: some View {
-//        VStack {
-//            Spacer()
-//            VStack(spacing: 15) {
-//                AsyncImage(url: url) { returnedImage in
-//                    returnedImage.resizable()
-//                        .scaledToFit()
-//                        .frame(width: 200, height: 250)
-//                } placeholder: {
-//                    ProgressView()
-//                }
-//
-//                Text("Persuasion")
-//                    .font(.title3)
-//            }
-//            Spacer()
-//            Text(bookDescriptions[1]!)
-//                .multilineTextAlignment(.center)
-//                .lineSpacing(10)
-//                .padding(.horizontal)
-//                .font(.headline)
-//            Spacer()
-//            Spacer()
-//        }
-//    }
-//}
-//
-//struct EmmaDetailView: View {
-//    @ObservedObject var viewModel = WorksViewModel()
-//    let url = URL(string: "https://books.google.com/books/content?id=6-ndr-0EMycC&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api")
-//
-//    var body: some View {
-//        VStack {
-//            Spacer()
-//            VStack(spacing: 15) {
-//                AsyncImage(url: url) { returnedImage in
-//                    returnedImage.resizable()
-//                        .scaledToFit()
-//                        .frame(width: 200, height: 250)
-//                } placeholder: {
-//                    ProgressView()
-//                }
-//
-//                Text("Emma")
-//                    .font(.title3)
-//            }
-//            Spacer()
-//            Text(bookDescriptions[2]!)
-//                .multilineTextAlignment(.center)
-//                .lineSpacing(10)
-//                .padding(.horizontal)
-//                .font(.headline)
-//            Spacer()
-//            Spacer()
-//        }
-//    }
-//}
-//
-//struct SenseandSensibilityDetailView: View {
-//    @ObservedObject var viewModel = WorksViewModel()
-//    let url = URL(string: "https://books.google.com/books/content?id=P2h8CtM-pFEC&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api")
-//
-//    var body: some View {
-//        VStack {
-//            Spacer()
-//            VStack(spacing: 15) {
-//                AsyncImage(url: url) { returnedImage in
-//                    returnedImage.resizable()
-//                        .scaledToFit()
-//                        .frame(width: 200, height: 250)
-//                } placeholder: {
-//                    ProgressView()
-//                }
-//
-//                Text("Sense and Sensibility")
-//                    .font(.title3)
-//            }
-//            Spacer()
-//            Text(bookDescriptions[3]!)
-//                .multilineTextAlignment(.center)
-//                .lineSpacing(10)
-//                .padding(.horizontal)
-//                .font(.headline)
-//            Spacer()
-//            Spacer()
-//        }
-//    }
-//}
-//
-//struct MansfieldParkDetailView: View {
-//    @ObservedObject var viewModel = WorksViewModel()
-//    let url = URL(string:     "https://books.google.com/books/content?id=krnu-3b88JgC&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api")
-//
-//    var body: some View {
-//        VStack {
-//            Spacer()
-//            VStack(spacing: 15) {
-//                AsyncImage(url: url) { returnedImage in
-//                    returnedImage.resizable()
-//                        .scaledToFit()
-//                        .frame(width: 200, height: 250)
-//                } placeholder: {
-//                    ProgressView()
-//                }
-//
-//                Text("Mansfield Park")
-//                    .font(.title3)
-//            }
-//            Spacer()
-//            Text(bookDescriptions[4]!)
-//                .multilineTextAlignment(.center)
-//                .lineSpacing(10)
-//                .padding(.horizontal)
-//                .font(.headline)
-//            Spacer()
-//            Spacer()
-//        }
-//    }
-//}
-//
-//struct NorthangerAbbeyDetailView: View {
-//    @ObservedObject var viewModel = WorksViewModel()
-//    let url = URL(string:     "https://books.google.com/books/content?id=-Ggriu3ENI0C&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api")
-//
-//    var body: some View {
-//        VStack {
-//            Spacer()
-//            VStack(spacing: 15) {
-//                AsyncImage(url: url) { returnedImage in
-//                    returnedImage.resizable()
-//                        .scaledToFit()
-//                        .frame(width: 200, height: 250)
-//                } placeholder: {
-//                    ProgressView()
-//                }
-//
-//                Text("Northanger Abbey")
-//                    .font(.title3)
-//            }
-//            Spacer()
-//            Text(bookDescriptions[5]!)
-//                .multilineTextAlignment(.center)
-//                .lineSpacing(10)
-//                .padding(.horizontal)
-//                .font(.headline)
-//            Spacer()
-//            Spacer()
-//        }
-//    }
-//}
