34c140fd040fa01cbd7dd525300a09452a7ea4df
Jeff Licona <57239224+jeffala@users.noreply.github.com>
2022-04-29 23:23:28 -0400

    adding better book summaries #2

--- JANE/BooksData.swift
+++ JANE/BooksData.swift
@@ -42,60 +42,88 @@ struct JaneAustenBooks {
             descSourceCitation: "“Mansfield Park.” Wikipedia, Wikimedia Foundation, 30 Mar. 2022, https://en.wikipedia.org/wiki/Mansfield_Park."),
         
         Book(title: "Northanger Abbey",
-             description: "During an eventful season at Bath, young, naïve Catherine Morland experiences the joys of fashionable society for the first time. She is delighted with her new acquaintances: flirtatious Isabella, who shares Catherine's love of Gothic romance and ...",
-             imageURL: URL(string:  "https://books.google.com/books/content?id=-Ggriu3ENI0C&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api")!)
+             description: """
+             Seventeen-year-old Catherine Morland is one of ten children of a country clergyman. Although a tomboy in her childhood, by the age of 17 she is "in training for a heroine" and is fond of reading Gothic novels, "provided they were all story and no reflection".
+             
+             Catherine is invited by the Allens (her wealthier neighbours in Fullerton) to accompany them to visit the city of Bath and partake in the winter season of balls, theatre and other social delights. Soon she is introduced to a clever young gentleman, Henry Tilney, with whom she dances and converses. Through Mrs. Allen's old schoolfriend Mrs. Thorpe, she meets her daughter Isabella, who introduces Catherine to Ann Radcliffe's Mysteries of Udolpho, and the two quickly become friends. Mrs. Thorpe's son, John, is also a friend of Catherine's older brother, James, at Oxford where they are both students.
+             """,
+             imageURL: URL(string:  "https://books.google.com/books/content?id=-Ggriu3ENI0C&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api")!,
+             sourceOfDesc: "https://en.wikipedia.org/wiki/Northanger_Abbey",
+             descSourceCitation: "“Northanger Abbey.” Wikipedia, Wikimedia Foundation, 28 Apr. 2022, https://en.wikipedia.org/wiki/Northanger_Abbey."),
     ]
 }
 
 struct IvanTurgenevBooks {
     static let bestWorks = [
         Book(title: "A Sportsman's Sketches",
-             description: "A Sportsman's Sketches; also known as The Hunting Sketches and Sketches from a Hunter's Album, was an 1852 collection of short stories by Ivan Turgenev. It was the first major writing that gained him recognition. He wrote this collection of short ...",
-             imageURL: URL(string: "https://books.google.com/books/content?id=rh4yvgAACAAJ&printsec=frontcover&img=1&zoom=1&source=gbs_api")!),
+             description: "Collection of short stories by Ivan Turgenev published in Russian as Zapiski okhotnika in 1852; additional stories were included in the 1870s. The collection has also been translated as Sketches from a Hunter’s Album and A Sportsman’s Notebook.",
+             imageURL: URL(string: "https://books.google.com/books/content?id=rh4yvgAACAAJ&printsec=frontcover&img=1&zoom=1&source=gbs_api")!,
+            sourceOfDesc: "https://www.britannica.com/topic/A-Sportsmans-Sketches",
+            descSourceCitation: "“A Sportsman's Sketches.” Encyclopædia Britannica, Encyclopædia Britannica, Inc., https://www.britannica.com/topic/A-Sportsmans-Sketches."),
         
         Book(title: "Rudin",
-             description: "Reproduction of the original: Rudin by Ivan Turgenev",
-             imageURL: URL(string: "https://books.google.com/books/content?id=YYtRDwAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api")!),
+             description: "Rudin was written by Turgenev in the immediate aftermath of the Crimean War, when it became obvious to many educated Russians that reform was needed. The main debate of Turgenev's own generation was that of Slavophiles versus Westernizers. Rudin depicts a typical man of this generation (known as 'the men of forties'), intellectual but ineffective. This interpretation of the superfluous man as someone who possesses great intellectual ability and potential, but is unable to realize them stems from Turgenev's own view of human nature, expressed in his 1860 speech ‘Hamlet and Don Quixote’, where he contrasts egotistical Hamlet, too deep in reflection to act, and enthusiastic and un-thinking, but active Don Quixote. The main character of the novel, Rudin, is easily identified with Hamlet.",
+             imageURL: URL(string: "https://books.google.com/books/content?id=YYtRDwAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api")!,
+            sourceOfDesc: "https://en.wikipedia.org/wiki/Rudin",
+            descSourceCitation: "“Rudin.” Wikipedia, Wikimedia Foundation, 17 Mar. 2022, https://en.wikipedia.org/wiki/Rudin."),
         
         Book(title: "On the Eve",
-             description: "No description available",
-             imageURL: URL(string: "https://books.google.com/books/content?id=9tJKAAAAYAAJ&printsec=frontcover&img=1&zoom=1&source=gbs_api")!),
+             description: "The story revolves around Elena Stakhova, a girl with a hypochondriac mother and an idle father, a retired guards lieutenant with a mistress. On the eve of the Crimean War, Elena is pursued by a free-spirited sculptor (Pavel Shubin) and a serious-minded student (Andrei Berzyenev). But when Berzyenev's revolutionary Bulgarian friend, Dmitri Insarov, meets Elena, they fall in love. In secretly marrying Insarov Elena disappoints her mother and enrages her father, who had hoped to marry her to a dull, self-satisfied functionary, Kurnatovski. Insarov nearly dies from pneumonia and only partly recovers. On the outbreak of war Insarov tries to return with Elena to Bulgaria, but dies in Venice. Elena takes Insarov's body to the Balkans for burial and then vanishes.",
+             imageURL: URL(string: "https://books.google.com/books/content?id=9tJKAAAAYAAJ&printsec=frontcover&img=1&zoom=1&source=gbs_api")!,
+            sourceOfDesc: "https://en.wikipedia.org/wiki/On_the_Eve",
+            descSourceCitation: "“On the Eve.” Wikipedia, Wikimedia Foundation, 17 Mar. 2022, https://en.wikipedia.org/wiki/On_the_Eve."),
         
         Book(title: "Fathers and Sons",
-             description: "Turgenev's masterpiece about the conflict between generations is as fresh, outspoken, and exciting today as it was in when it was first published in 1862. The controversial portrait of Bazarov, the energetic, cynical, and self-assured `nihilist ...",
-             imageURL: URL(string: "https://books.google.com/books/content?id=AKlFPgAACAAJ&printsec=frontcover&img=1&zoom=1&source=gbs_api")!),
+             description: """
+             Arkady Kirsanov has just graduated from the University of Petersburg. He returns with a friend, Bazarov, to his father's modest estate in an outlying province of Russia. His father, Nikolay, gladly receives the two young men at his estate, called Marino, but Nikolay's brother, Pavel, soon becomes upset by the strange new philosophy called "nihilism" which the young men, especially Bazarov, advocate.
+             
+             Nikolay, initially delighted to have his son return home, slowly begins to feel uneasy. A certain awkwardness develops in his regard toward his son, as Arkady's radical views, much influenced by Bazarov, make Nikolay’s own beliefs feel dated. Nikolay has always tried to stay as current as possible, by doing things such as visiting his son at school so the two can stay as close as they are, but this in Nikolay's eyes has failed. To complicate this, the father has taken a servant, Fenechka, into his house to live with him and has already had a son by her, named Mitya. Arkady, however, is not troubled by the relationship: to the contrary, he openly celebrates the addition of a younger brother.
+             """,
+             imageURL: URL(string: "https://books.google.com/books/content?id=AKlFPgAACAAJ&printsec=frontcover&img=1&zoom=1&source=gbs_api")!,
+            sourceOfDesc: "https://en.wikipedia.org/wiki/Fathers_and_Sons_(novel)",
+            descSourceCitation: "“Fathers and Sons (Novel).” Wikipedia, Wikimedia Foundation, 12 Apr. 2022, https://en.wikipedia.org/wiki/Fathers_and_Sons_(novel)."),
         
         Book(title: "Home of the Gentry",
-             description: "On one level the novel is about the homecoming of Lavretsky, who, broken and disillusioned by a failed marriage, returns to his estate and finds love again - only to lose it. The sense of loss and of unfulfilled promise, beautifully captured by ...",
-             imageURL: URL(string: "https://books.google.com/books/content?id=uiOiHCx7ANoC&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api")!),
-        
-        Book(title: "A Reckless Character and Other Stories",
-             description: "No description available",
-             imageURL: URL(string: "https://books.google.com/books/content?id=NzZtswEACAAJ&printsec=frontcover&img=1&zoom=1&source=gbs_api")!),
+             description: """
+             "The novel's protagonist is Fyodor Ivanych Lavretsky, a nobleman who shares many traits with Turgenev. The child of a distant, Anglophile father and a serf mother who dies when he is very young, Lavretsky is brought up at his family's country estate home by a severe maiden aunt, often thought to be based on Turgenev's own mother, who was known for her cruelty.
+             
+             Lavretsky pursues an education in Moscow, and while he is studying there, he spies a beautiful young woman at the opera. Her name is Varvara Pavlovna, and he falls in love with her and asks for her hand in marriage. Following their wedding, the two move to Paris, where Varvara Pavlovna becomes a very popular salon hostess and begins an affair with one of her frequent visitors. Lavretsky learns of the affair only when he discovers a note written to her by her lover. Shocked by her betrayal, he severs all contact with her and returns to his family estate.
+             """,
+             imageURL: URL(string: "https://books.google.com/books/content?id=uiOiHCx7ANoC&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api")!,
+            sourceOfDesc: "https://en.wikipedia.org/wiki/Home_of_the_Gentry",
+            descSourceCitation: "“Home of the Gentry.” Wikipedia, Wikimedia Foundation, 17 Mar. 2022, https://en.wikipedia.org/wiki/Home_of_the_Gentry."),
         
         Book(title: "First Love",
-             description: "Isaiah Berlin's translation of the legendary Russian novella of growing up and heartbreak When the down-at-heel Princess Zasyekin moves next door to the country estate of Vladimir Petrovich's parents, he instantly and overwhelmingly falls in love ...",
-             imageURL: URL(string: "https://books.google.com/books/content?id=3xNp_TodsBwC&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api")!),
+             description: "Ivan Turgenev’s 1860 novella charts the course of a 16-year old boy’s infatuation with the princess next door in the summer of 1833, while on holiday at the family dacha just outside Moscow.  What could have been trite, in Turgenev’s hands achieves universal significance as he depicts the powerful emotions experienced by someone who is leaving childhood behind and entering the world of adult relationships, with all their joys and heartbreaks.  The title suggests a light summer romance of the ‘old enough to know better, young enough not to care’ variety, but Turgenev instead delivers a powerful dissection of the infatuations of youth.",
+             imageURL: URL(string: "https://books.google.com/books/content?id=3xNp_TodsBwC&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api")!,
+            sourceOfDesc: "https://tomruffles.wordpress.com/2017/09/22/first-love-by-ivan-turgenev/",
+            descSourceCitation: "“First Love, by Ivan Turgenev.” The Joy of Mere Words, 22 Sept. 2017, https://tomruffles.wordpress.com/2017/09/22/first-love-by-ivan-turgenev/."),
         
         Book(title: "Spring Torrents",
-             description: "Returning to Russia from a tour in Italy, twenty-three-year-old Dimitry Sanin breaks his journey in Frankfurt. There he encounters the beautiful Gemma Roselli, who works in her parents' patisserie, and falls deeply and deliriously in love for the ...",
-             imageURL: URL(string: "https://books.google.com/books/content?id=fGCyDwAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api")!),
-        
-        Book(title: "King Lear of the Steppes",
-             description: "First published in 1870 ‘King Lear of the Steppes’ is a novella by Russian writer Ivan Turgenev, loosely based on Shakespeare’s tragedy. The story takes place in the Russian countryside, where a trusting father decides to retire and divide his ...",
-             imageURL: URL(string: "https://books.google.com/books/content?id=QaU0EAAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api")!)
+             description: """
+             The story opens with a middle-aged Dmitry Sanin rummaging through the papers in his study when he comes across a small cross set with garnets, which sends his thoughts back thirty years to 1840.
+
+             In the summer of 1840, a twenty-two-year-old Sanin, arrives in Frankfurt en route home to Russia from Italy at the culmination of a European tour. During his one-day layover he visits a confectioner's shop where he is rushed upon by a beautiful young woman who emerges frantic from the back room. She is Gemma Roselli, the daughter of the shop's proprietress, Leonora Roselli. Gemma implores Sanin to help her younger brother who has passed out and seems to have stopped breathing. Thanks to Sanin's aid, the boy – whose name is Emilio – emerges from his faint. Grateful for his assistance, Gemma invites Sanin to return to the shop later in the evening to enjoy a cup of chocolate with the family.
+             """,
+             imageURL: URL(string: "https://books.google.com/books/content?id=fGCyDwAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api")!,
+            sourceOfDesc: "https://en.wikipedia.org/wiki/Torrents_of_Spring",
+            descSourceCitation: "“Torrents of Spring.” Wikipedia, Wikimedia Foundation, 9 Apr. 2022, https://en.wikipedia.org/wiki/Torrents_of_Spring. "),
     ]
 }
 
 struct GeorgeOrwellBooks {
     static let bestWorks = [
         Book(title: "Nineteen Eighty-four",
-             description: "A chilling portrait of a totalitarian society under the ever-watchful gaze of Big Brother, where love, privacy, and individuality are banned. The year 1984 has come and gone, but George Orwell's nightmare vision from 1949 of the world we are ...",
-             imageURL: URL(string: "https://books.google.com/books/content?id=lbqlAQAACAAJ&printsec=frontcover&img=1&zoom=1&source=gbs_api")!),
+             description: "The story takes place in an imagined future, the year 1984, when much of the world has fallen victim to perpetual war, omnipresent government surveillance, historical negationism, and propaganda. Great Britain, known as Airstrip One, has become a province of the totalitarian superstate Oceania, ruled by the Party, who employ the Thought Police to persecute individuality and independent thinking.[5] Big Brother, the dictatorial leader of Oceania, enjoys an intense cult of personality, manufactured by the party's excessive brainwashing techniques. The protagonist, Winston Smith, is a diligent and skillful rank-and-file worker at the Ministry of Truth and Outer Party member who secretly hates the Party and dreams of rebellion. He expresses his dissent by writing in a diary and later enters into a forbidden relationship with his colleague Julia and starts to remember what life was like before the Party came to power.",
+             imageURL: URL(string: "https://books.google.com/books/content?id=lbqlAQAACAAJ&printsec=frontcover&img=1&zoom=1&source=gbs_api")!,
+            sourceOfDesc: "https://en.wikipedia.org/wiki/Nineteen_Eighty-Four",
+            descSourceCitation: "“Nineteen Eighty-Four.” Wikipedia, Wikimedia Foundation, 27 Apr. 2022, https://en.wikipedia.org/wiki/Nineteen_Eighty-Four. "),
         
         Book(title: "Animal Farm",
-             description: "George Orwell was a much-respected English novelist, who wrote some of the finest pieces in literary criticism, fiction. Orwell's work is known for its simplicity and wit. He wrote with smartness on subjects such as anti-fascism, democratic ...",
-             imageURL: URL(string: "https://books.google.com/books/content?id=tJf8DwAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api")!),
+             description: "The animals get fed up of their master, Farmer Jones, so they kick him out. Once they are free of the tyrant Jones, life on the farm is good for a while and there is hope for a happier future of less work, better education and more food. However, trouble brews as the pigs, Napoleon and Snowball, fight for the hearts and minds of the other animals on the farm. Napoleon seizes power by force and ends up exploiting the animals just as Farmer Jones had done. The novel ends with the pigs behaving and even dressing like the humans the animals tried to get rid of in the first place.",
+             imageURL: URL(string: "https://books.google.com/books/content?id=tJf8DwAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api")!,
+            sourceOfDesc: "https://www.bbc.co.uk/bitesize/guides/z9w7mp3/revision/1",
+             descSourceCitation: "“Animal Farm - Plot Summary - Plot Summary - GCSE English Literature Revision - BBC Bitesize.” BBC News, BBC, https://www.bbc.co.uk/bitesize/guides/z9w7mp3/revision/1."),
         
         Book(title: "A Clergyman’s Daughter",
              description: "A pious young woman grapples with a loss of memory—and of faith—in this sharp, witty novel by the author of 1984 and Animal Farm. Dorothy is the daughter of the Reverend Charles Hare, rector of St. Athelstan’s in Depression-era Suffolk, England ...",
